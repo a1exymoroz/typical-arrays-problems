5af3125fc7e3cb73e3b37766e664311b86ee0449
Aleksei Morozov <aleksei.morozov@mera.com>
2020-02-20 09:29:23 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    let sum = array.reduce((acu, cur) => acu += cur, 0)
+    return sum/array.length;
 }
